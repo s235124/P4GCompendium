e0ef5e0f8d1555c1f45e205a317a60997bc2166b
Zia Beg <ziabeg@hotmail.com>
2024-10-10 14:07:24 +0200

    first commit

---
+++ app/src/main/java/com/example/p4g/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.p4g
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.p4g.ui.theme.P4GTheme
+
+private data class Persona (
+    val id: Int,
+    val name: String,
+    val level: Int,
+    val arcana: String,
+    val primaryElement: String,
+    val elements: MutableList<String>
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P4GTheme {
+                P4GCompendiumApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun P4GCompendiumApp () {
+    MainPage()
+}
+
+@Composable
+fun MainPage (modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize()
+) {
+
+}
